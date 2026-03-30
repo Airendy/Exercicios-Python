57016a66a2e82e5685df1561360c538b19c9d245
Rafaela Becker <65984117+Airendy@users.noreply.github.com>
2026-03-29 21:47:10 -0300

    Create atividade15.py

---
+++ atividade15.py
@@ -0,0 +1,7 @@
+#Leia o nome e a idade e imprima exatamente neste formato: 
+#Nome: <nome> | Idade: <idade> anos
+
+nome = input("Nome: ")
+idade = int(input("Idade: "))
+
+print(f"Nome: {nome} | Idade: {idade} anos")
